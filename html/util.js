c35e950474ebf209fe50e7fcbfb2afa92bb39d27
Slash0BZ <zhouxuanyu.personal@gmail.com>
2017-11-29 20:56:16 +0000

    save temp work

--- html/util.js
+++ html/util.js
@@ -25,11 +25,23 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+function eraseCookieFromAllPaths(name) {
+    // This function will attempt to remove a cookie from all paths.
+    var pathBits = location.pathname.split('/');
+    var pathCurrent = ' path=';
+
+    // do a simple pathless delete first.
+    document.cookie = name + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT;';
+
+    for (var i = 0; i < pathBits.length; i++) {
+        pathCurrent += ((pathCurrent.substr(-1) != '/') ? '/' : '') + pathBits[i];
+        document.cookie = name + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT;' + pathCurrent + ';';
+    }
+}
+
 function logout(){
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++)
-      deleteCookie(cookies[i].split("=")[0]);
-    window.location = "http://groupspaceuiuc.com/index.html";
+    eraseCookieFromAllPaths("username");
+    //window.location = "http://groupspaceuiuc.com/index.html";
  }
 
 function getRequests() {
